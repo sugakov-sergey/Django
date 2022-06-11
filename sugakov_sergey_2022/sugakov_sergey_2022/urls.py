bef61ede040e4335b9032c2c0ef05685cd37e3ba
Sugakov Sergey <sugakov@sanline.by>
2022-06-11 12:07:30 +0300

    remake header

--- sugakov_sergey_2022/sugakov_sergey_2022/urls.py
+++ sugakov_sergey_2022/sugakov_sergey_2022/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project.urls')),
     path('blog/', include('blog.urls')),
+    path('base/', views.base),
 ]
